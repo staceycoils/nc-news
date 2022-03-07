71b6d65f55eb658a06d0b71136515e8d99356461
Stacey <staceyelsom@gmail.com>
2022-03-07 20:56:27 +0000

    Added temp link on '/' to articles page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
 
   return (
     <div>
-        Home 
+        Home<br />
+        <Link to="/articles" >Articles</Link>
     </div>
   )
 }
