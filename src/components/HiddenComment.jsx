41680dc18010ffcdf2776374af1ec1f4eaa3ca4b
Stacey <staceyelsom@gmail.com>
2022-03-11 09:36:32 +0000

    Added ability to show a hidden comment

--- src/components/HiddenComment.jsx
+++ src/components/HiddenComment.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
+
 export default function HiddenComment(props) {
+  const [reveal, setReveal] = useState("")
   const {comment} = props
   
   if (comment.hide) {
   return (
       <p className="ArticleBody">
-        <b>This comment has been hidden due to low score.</b>
+        <b>This comment has been hidden due to low score. </b> 
+        <button onClick={()=>{setReveal(`${comment.body}`)}}>Show</button><br/>
+        {reveal}
       </p>
   )
   
