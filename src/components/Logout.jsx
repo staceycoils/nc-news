bcf72c18c8e0540cc4f018ef6966f4c963fc0e42
Stacey <staceyelsom@gmail.com>
2022-04-24 12:16:23 +0100

    Moved login/logout to two seperate pages, allowing to enter username to login

---
+++ src/components/Logout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useContext } from 'react'
+import { UserContext } from '../contexts/UserContext'
+
+export default function Logout() {
+  const { user, setUser } = useContext(UserContext)
+
+  function logoutUser(e) {
+    setUser(null)
+    window.alert("You have been logged out")
+  }
+
+  return (
+    <button onClick={(e)=>{logoutUser(e)}}>
+        LOGOUT
+    </button>
+  )
+}
